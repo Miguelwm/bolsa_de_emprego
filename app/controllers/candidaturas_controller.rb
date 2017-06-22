9d881787fa0ff0607c90e343c0b2d038aeacc275
Miguelwm <miguelpereira.wm@gmail.com>
2017-06-22 16:58:28 +0100

    Utilização de ajax no botão de candidatar

--- app/controllers/candidaturas_controller.rb
+++ app/controllers/candidaturas_controller.rb
@@ -2,9 +2,13 @@ class CandidaturasController < ApplicationController
   before_action :is_candidato?
 
   def create
-    oferta = Oferta.find(params[:oferta_id])
-    Newsletter.candidatura(oferta,conta_atual)
-    candidato_atual.candidatar(oferta)
-    redirect_to oferta
+    @oferta = Oferta.find(params[:oferta_id])
+    Newsletter.candidatura(@oferta,conta_atual)
+    candidato_atual.candidatar(@oferta)
+
+    respond_to do |format|
+      format.html { redirect_to @oferta }
+      format.js
+    end
   end
 end
